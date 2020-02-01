b0a2eaa2b6814f93ec14123cb33c33030e4ad540
zsq259 <2530059525@qq.com>
2020-02-01 13:10:38 +0800

    Site updated: 2020-02-01 13:10:38

--- js/schemes/cheat.js
+++ js/schemes/cheat.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '您不要我了么QwQ';
+         document.title = '您不要我了么?QwQ';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '您又回来啦!' + OriginTitle;
+         document.title = '您又回来啦!^_^';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
